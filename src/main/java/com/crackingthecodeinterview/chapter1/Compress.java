058ff5815cd209ee2702d546838073ecb6b57e17
s.akhlaghi <sinaakhlaqi@yahoo.com>
2024-04-24 09:29:52 +0330

    Making all suggested modifications

--- src/main/java/com/crackingthecodeinterview/chapter1/Compress.java
+++ src/main/java/com/crackingthecodeinterview/chapter1/Compress.java
@@ -1,33 +1,59 @@
 package com.crackingthecodeinterview.chapter1;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Compress {
-    //1.6 String Compression: Implement a method to perform basic string compression using the counts
-    //    of repeated characters. For example, the string aabcccccaaa would become a2blc5a3. If the
-    //    "compressed" string would not become smaller than the original string, your method should return
-    //    the original string. You can assume the string has only uppercase and lowercase letters (a - z).
-    public static String compress(String str) {
-        if (str == null)
-            return null;
+
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+    }
+
+    /**
+     * Returns a compact representation of input string with respect to character frequency
+     * in the input string.
+     * <em>
+     * this api try's to solve problem 6
+     * <p>
+     * Problem String Compression: Implement a method to perform basic string compression using the counts
+     * of repeated characters. For example, the string aabcccccaaa would become a2blc5a3. If the
+     * "compressed" string would not become smaller than the original string, your method should return
+     * the original string. You can assume the string has only uppercase and lowercase letters (a - z).
+     *
+     * @param inputString the input string
+     * @return the compressed string
+     * @throws NullPointerException if the inputString is null
+     * @example compress(aabcccccaaa) returns a2blc5a3;
+     */
+    public static String compress(String inputString) {
         StringBuilder compressed = new StringBuilder();
-        int charCount = 1;
+        int charCount;
         int i = 0;
-        while (i < str.length()) {
-            int j = i + 1;
-            while (j < str.length()) {
-                if (str.charAt(j) == str.charAt(i)) {
-                    charCount++;
-                    j++;
-                } else {
-                    break;
-                }
-            }
-            compressed.append(str.charAt(i));
+        while (i < inputString.length()) {
+            charCount = countRepeatedChar(inputString,
+                    inputString.charAt(i),
+                    i);
+            compressed.append(inputString.charAt(i));
             if (charCount > 1) {
                 compressed.append(charCount);
             }
-            i = j;
-            charCount = 1;
+            i = (charCount == 0) ? i + 1 : i + charCount;
         }
-        return compressed.length() < str.length() ? compressed.toString() : str;
+        return compressed.length() < inputString.length() ? compressed.toString() : inputString;
     }
+
+
+    // helpers
+    public static int countRepeatedChar(String inputString, char repeatedChar, int startIndex) {
+        int countOfChar = 0;
+        int charIndex = startIndex;
+        while (charIndex < inputString.length()
+                && (inputString.charAt(charIndex) == repeatedChar)) {
+            countOfChar++;
+            charIndex++;
+        }
+        return countOfChar;
+    }
+
 }
--- src/test/java/com/crackingthecodeinterview/chapter1/CompressTest.java
+++ src/test/java/com/crackingthecodeinterview/chapter1/CompressTest.java
@@ -2,21 +2,18 @@ package com.crackingthecodeinterview.chapter1;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static com.crackingthecodeinterview.chapter1.Compress.compress;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CompressTest {
     //compress test success case;
     @Test
-    void compress_successfully() {
+    void testCompress_onSmallerResult_returnCompressed() {
         String inputStr1 = "abbbaab";
-        String inputStr2 = "ab";
-        String inputStr3 = "a";
-
         assertThat(compress(inputStr1)).isEqualTo("ab3a2b");
-        assertThat(compress(inputStr2)).isEqualTo("ab");
-        assertThat(compress(inputStr3)).isEqualTo("a");
-        assertNull(compress(null));
+    }@Test
+    void testCompress_onNotSmallerResult_returnOriginal() {
+        String inputStr = "abababab";
+        assertThat(compress(inputStr)).isEqualTo("abababab");
     }
 }
